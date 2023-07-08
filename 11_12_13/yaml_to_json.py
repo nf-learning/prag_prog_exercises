12a4c5eeef346c6682e8d38de5102914fe58abd2
Nigel Faria <nigel.faria@gmail.com>
2023-07-08 20:43:49 +0100

    Fix: Take directory name as argument

--- 11_12_13/yaml_to_json.py
+++ 11_12_13/yaml_to_json.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 import json
@@ -10,11 +11,18 @@ def convert_yaml_to_json(yaml_file, json_file):
         json.dump(yaml_data, file, indent=4)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python yaml_to_json.py <input_yaml_file> <output_json_file>")
+    if len(sys.argv) < 2:
+        print("Usage: python yaml_to_json.py <dir>")
         sys.exit(1)
 
-    yaml_file = sys.argv[1]
-    json_file = sys.argv[2]
+    directory_path = sys.argv[1]
+    if not os.path.isdir(directory_path):
+        print("Error: Directory does not exist.")
+        sys.exit(1)
 
-    convert_yaml_to_json(yaml_file, json_file)
+    for file_name in os.listdir(directory_path):
+        if file_name.endswith('.yaml') or file_name.endswith('.yml'):
+            yaml_file = os.path.join(directory_path, file_name)
+            json_file = os.path.join(directory_path, file_name.rsplit('.', 1)[0] + '.json')
+            convert_yaml_to_json(yaml_file, json_file)
+
